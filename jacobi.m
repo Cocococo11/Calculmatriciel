fb548246b7827daf6fcd5d45355add9d32236b0f
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2019-10-01 17:15:20 +0200

    Add files via upload

--- jacobi.m
+++ jacobi.m
@@ -2,31 +2,34 @@ function [X] = jacobi(A,B)
 %JACOBI Résolution de système par méthoded Jacobi
 %   
 M = diag(diag(A)); 
-X = M ;
+
+somme=0;
 
 taille = size(B);
 l = taille(1,1);
 X0 = ones(l,1);
 
-while (min(abs(A.*X0-B))>=0.01)
+
+
+while (min(abs((A.*X0)-B))>=0.0001)
     for i=1:l
-        for j=1:l
         Xt = zeros(l,1);
-        somme = A(i,j).*X0(j,1) ;   
-        Xt(i,1)=(B(i,1)-somme)/A(i,i);
+        for j=1:l 
+            if (j~=i)
         
+                somme = A(i,j).*X0(j,1) ;
+            end
         end
+        Xt(i,1)=(B(i,1)-somme)./A(i,i);
+        
+        
     end
     X0 = Xt;
 end
-    
-    
 
-
-
-
-
-
+X=X0;
 
 end
+
+
 
