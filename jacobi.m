400cac27ab7fd94c1126305e2775fd8c1f4f21e7
Cocococo11 <56036274+Cocococo11@users.noreply.github.com>
2019-10-01 16:59:04 +0200

    Add files via upload

--- jacobi.m
+++ jacobi.m
@@ -6,13 +6,13 @@ X = M ;
 
 taille = size(B);
 l = taille(1,1);
-X0 = rand(1,l);
+X0 = ones(l,1);
 
-while (min(abs(A.*X0-B))>=0.000001)
+while (min(abs(A.*X0-B))>=0.01)
     for i=1:l
         for j=1:l
         Xt = zeros(l,1);
-        somme = A(i,j)*X0(j,1) ;   
+        somme = A(i,j).*X0(j,1) ;   
         Xt(i,1)=(B(i,1)-somme)/A(i,i);
         
         end
